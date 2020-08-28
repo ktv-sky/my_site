1f419058413ea8fa2d4eb33667946168e9b7bc57
ktv-sky <helicooper7@gmail.com>
2020-08-28 23:10:49 +0300

    Add editing forms and templates

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic, Entry
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -35,4 +35,40 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('blog:topics'))
     return render(request, 'blog/new_topic.html', {
         'form': form
+    })
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(
+                reverse('blog:topic', args=[topic_id])
+                )
+    return render(request, 'blog/new_entry.html', {
+        'topic': topic,
+        'form': form,
+    })
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(
+                reverse('blog:topic', args=[topic.id])
+                )
+    return render(request, 'blog/edit_entry.html', {
+        'entry': entry,
+        'topic': topic,
+        'form': form,
     })
