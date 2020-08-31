ac9383f4072a92ca0ebbfa62653ff47e52364b26
ktv-sky <helicooper7@gmail.com>
2020-08-31 11:50:58 +0300

    http404 fix, and redirect instead of httpresponseredirect

--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
-from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
 
 
 def login_view(request):
@@ -12,7 +10,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return HttpResponseRedirect(reverse('blog:index'))
+            return redirect('blog:index')
         else:
             return render(request, 'users/login.html', {
                 'message': 'Invalid Credentials',
