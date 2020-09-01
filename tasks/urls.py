fa92f393ec4221c75cc85e6905faee0be640b37f
ktv-sky <helicooper7@gmail.com>
2020-09-01 13:01:25 +0300

    Add Tasks and change styles

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = 'tasks'
 urlpatterns = [
-    path('tasks/', views.tasks, name='tasks'),
+    path('', views.tasks, name='tasks'),
+    path('new_task/', views.new_task, name='new_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
 from .models import Task
+from .forms import TaskForm
 
 
 @login_required
@@ -10,4 +11,20 @@ def tasks(request):
     tasks = Task.objects.filter(owner=request.user).order_by('-date_added')
     return render(request, 'tasks/tasks.html', {
         'tasks': tasks
+    })
+
+@login_required
+def new_task(request):
+    if request.method != 'POST':
+        form = TaskForm()
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            new_task = form.save(commit=False)
+            new_task.owner = request.user
+            new_task.save()
+            return redirect('tasks:tasks')
+
+    return render(request, 'tasks/new_task.html', {
+        'form': form
     })
